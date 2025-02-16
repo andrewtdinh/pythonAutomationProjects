ead46da82edc6c9d1e8bd5e703c16db99bdc7c17
Andrew Dinh <andrewdinh74@yahoo.com>
2025-02-16 08:44:57 -0800

    Open existing google spreadsheet and update it

--- gsheets_automation/gspread_101.py
+++ gsheets_automation/gspread_101.py
@@ -3,3 +3,7 @@ import gspread
 g_client = gspread.service_account('service_account_creds.json')
 
 spread_sheet = g_client.open('gspread_101')
+
+ws = spread_sheet.sheet1
+
+ws.update([['Hello Googlesheets!']], 'A2')
